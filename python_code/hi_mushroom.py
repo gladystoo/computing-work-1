ff31d5efc02621a96f0dbcddee022a9a8fe5db53
Mantou3 <69775915+Mantou3@users.noreply.github.com>
2020-09-14 09:11:47 +0800

    Create hi_mushroom.py

---
+++ python_code/hi_mushroom.py
@@ -0,0 +1,6 @@
+#This code should scroll the words 'Hi,mushroom:)'across the micro:bit.
+#But there is an error!
+#Can you figure out how to fix it?:)))))
+
+from microbit import*
+display.scroll("Hi,mushroom:))
